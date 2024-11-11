c58559fd9a72f8cf7bf984f781085d2f452c3af3
Sunlight-Warrior <gracafigueiro@outlook.com>
2024-11-11 00:21:14 -0300

    sobrenos.php

--- Projeto/sobrenos.php
+++ Projeto/sobrenos.php
@@ -1,99 +1,79 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-
     <meta charset="UTF-8">
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous"
-    />
-
-    <link rel="stylesheet" type="text/css" href="css/sobrenos.css" />
-
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>Sobre nós</title>
+    <link rel="stylesheet" href="css/sobrenos.css">
+    <title>Sobre Nós</title>
 </head>
 <body>
-<?php
-    include './banco/sessionInfo/session.php';
-    ?>
-    <nav class="NavBar">
-        <div class="center">
-            <ul>
-                <li><a class="active" href="index.php">Home</a></li>
-                <li><a href="sobrenos.php">Sobre nós</a></li>
-                <?php
-                if ((isset($_SESSION['login']) == false)) {
-                    echo "<li style = 'float:right'><a href = './login_cadastro.php'>Cadastre-se</a></li>";
-                    echo "<li style = 'float:right'><a href = './login_page.php'>Entrar</a></li>";
-                } else {
-                    echo "<li style = 'float:right'><a </a><input class = 'link-button' form = 'btnSair' type = 'submit' value = 'Sair' name = 'btnSair'/></li>";
-                }
-    ?>
 
-            </ul>
-        </div>
-    </nav>
-        <!--Link abrindo outra janela guia-->
-        <!-- <div class="voltarHome img" >
-            <span class="tooltiptext">Voltar ao inicio</span>
-            <a href="./index.html"><img src="assets/img/novelo.svg" alt="..." /></a>
-        </div> -->
-    <div class="Background">  
-    <section class="page-section" id="about">
-        <div class="container">
-            <div class="text-center">
-            <br>
-                <h2 class="section-heading text-uppercase">Sobre Nós</h2>
-                <h3 class="section-subheading">Equipe</h3>
-            </div>
-            <ul class="timeline">
-                    <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/lazaro_animal.jpg" alt="..." /></div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4>Lázaro</h4>
-                            <h4 class="subheading"></h4>
-                        </div>
-                        <div class="timeline-body"><p class="">Estudante de Análise e Desenvolvimento de Sistemas, cursando primeiro período. <br> E-mail: lazaro.junior@outlook.com</p></div>
-                    </div>
-                    <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/Angel.jpeg" width="180px" alt="..." /></div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4>Matheus</h4>
-                            <h4 class="subheading"></h4>
-                        </div>
-                        <div class="timeline-body"><p class="">Estudante de Análise e Desenvolvimento de Sistemas, cursando segundo período. <br>"Este é o meu primeiro site, espero que fique bacana!" <br> E-mail: Gracafigueiro@outlook.com</p></div>
-                    </div>
-                    <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets\img\about\N.png" width="180px"  alt="..." /></div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4>Nicole</h4>
-                            <h4 class="subheading"></h4>
-                        </div>
-                        <div class="timeline-body"><p class="">Estudante de Análise e Desenvolvimento de Sistemas, cursando segundo período. <br> E-mail: nicolepsales@gmail.com <br></p></div>
-                    </div>
-                    <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/3.jpg" alt="..." /></div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4>Ruan</h4>
-                            <h4 class="subheading"></h4>
-                        </div>
-                        <div class="timeline-body"><p class="">Estudante de Ciência da Computação, cursando segundo período. <br> E-mail: Ruan_b3@hotmail.com </p></div>
-                    </div>
-                    <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/rui_animal.jpg" width="200px" alt="..." /></div>
-                    <div class="timeline-panel">
-                        <div class="timeline-heading">
-                            <h4>Rui</h4>
-                            <h4 class="subheading"></h4>
-                        </div>
-                        <div class="timeline-body"><p class="">Estudante de Análise e Desenvolvimento de Sistemas, cursando segundo período. <br> E-mail: ruigabriel765@gmail.com <br></p></div>
-                    </div>
-            </ul>
-        </div>
-    </section>
+<?php include './banco/sessionInfo/session.php'; ?>
+
+<nav class="NavBar">
+    <div class="center">
+        <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="sobrenos.php">Sobre nós</a></li>
+            <?php
+            if (!isset($_SESSION['login'])) {
+                echo "<li style='float:right'><a href='./login_cadastro.php'>Cadastre-se</a></li>";
+                echo "<li style='float:right'><a href='./login_page.php'>Entrar</a></li>";
+            } else {
+                echo "<li style='float:right'><a><input class='link-button' form='btnSair' type='submit' value='Sair' name='btnSair'/></a></li>";
+            }
+            ?>
+        </ul>
     </div>
+</nav>
+
+<section class="sobre-nos-section">
+    <h2 class="section-heading">Sobre Nós</h2>
+    <div class="team-container">
+        <div class="team-member">
+            <img src="assets/img/about/lazaro_animal.jpg" alt="Lázaro" class="member-img">
+            <div class="member-info">
+                <h3>Lázaro</h3>
+                <p>Estudante de Análise e Desenvolvimento de Sistemas, cursando primeiro período.</p>
+                <p>Email: lazaro.junior@outlook.com</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="assets/img/Angel.jpeg" alt="Matheus" class="member-img">
+            <div class="member-info">
+                <h3>Matheus</h3>
+                <p>Estudante de Análise e Desenvolvimento de Sistemas, cursando segundo período. "Este é o meu primeiro site, espero que fique bacana!"</p>
+                <p>Email: Gracafigueiro@outlook.com</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="assets/img/about/N.png" alt="Nicole" class="member-img">
+            <div class="member-info">
+                <h3>Nicole</h3>
+                <p>Estudante de Análise e Desenvolvimento de Sistemas, cursando segundo período.</p>
+                <p>Email: nicolepsales@gmail.com</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="assets/img/about/3.jpg" alt="Ruan" class="member-img">
+            <div class="member-info">
+                <h3>Ruan</h3>
+                <p>Estudante de Ciência da Computação, cursando segundo período.</p>
+                <p>Email: Ruan_b3@hotmail.com</p>
+            </div>
+        </div>
+        <div class="team-member">
+            <img src="assets/img/about/rui_animal.jpg" alt="Rui" class="member-img">
+            <div class="member-info">
+                <h3>Rui</h3>
+                <p>Estudante de Análise e Desenvolvimento de Sistemas, cursando segundo período.</p>
+                <p>Email: ruigabriel765@gmail.com</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="js/scroll-animation.js"></script>
+<!--<script src="js/confetti-animation.js"></script>!-->
 </body>
 </html>
