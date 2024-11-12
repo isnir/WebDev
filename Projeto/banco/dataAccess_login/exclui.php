8d4d4a8d31be14df6abd90e83e868ce741c66405
ruancaetano98 <ruan_b3@hotmail.com>
2024-11-12 20:47:13 -0300

    Update exclui.php

--- Projeto/banco/dataAccess_login/exclui.php
+++ Projeto/banco/dataAccess_login/exclui.php
@@ -5,5 +5,5 @@ $id = $_POST['id'];
 $sql = "delete from tblogin where id=$id ";
 $count = $con->exec($sql);
 echo "<script> alert(`$count registro foi removido!`); </script>";
-header('location:../../registro_login.php');
+header('location:../../database_login.php');
 
